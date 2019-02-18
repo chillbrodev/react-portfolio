2e65cf9e8fb1e7bde16b0b1af563dfcabfeb05f8
Jon Holtan <jonholtan@Alexanders-MacBook-Air.local>
2019-02-18 09:55:13 -0800

    Updates

---
+++ precache-manifest.e96ba6f89481e626d9f465dbd4d5d4de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "490089178a34f3ddfddea4f6e84a932c",
+    "url": "/react-portfolio/static/media/robot-rampage.49008917.png"
+  },
+  {
+    "revision": "9a8a00910af062955056",
+    "url": "/react-portfolio/static/css/main.925d9ce6.chunk.css"
+  },
+  {
+    "revision": "1e4ad457277730ac33c8",
+    "url": "/react-portfolio/static/js/runtime~main.1e4ad457.js"
+  },
+  {
+    "revision": "295ce26121a01d2622b2",
+    "url": "/react-portfolio/static/js/2.295ce261.chunk.js"
+  },
+  {
+    "revision": "0fb5dc9ee00b1a1f7fdde7b5f11827dc",
+    "url": "/react-portfolio/static/media/rx-java.0fb5dc9e.png"
+  },
+  {
+    "revision": "3074b65b3df16b7e178356c54fe1cbd0",
+    "url": "/react-portfolio/static/media/android-testing.3074b65b.png"
+  },
+  {
+    "revision": "6cb862322e4538eed0a56cb987f3fe8c",
+    "url": "/react-portfolio/static/media/planet-express.6cb86232.png"
+  },
+  {
+    "revision": "5e0fc88b48042ac7111e1366efffadbd",
+    "url": "/react-portfolio/static/media/mine-blast.5e0fc88b.png"
+  },
+  {
+    "revision": "9a8a00910af062955056",
+    "url": "/react-portfolio/static/js/main.9a8a0091.chunk.js"
+  },
+  {
+    "revision": "8b296f3f9f6c3306827bc604b8c94c77",
+    "url": "/react-portfolio/static/media/custom-views.8b296f3f.png"
+  },
+  {
+    "revision": "2663126b756613cac4c8b0a9526313a3",
+    "url": "/react-portfolio/static/media/militarium.2663126b.png"
+  },
+  {
+    "revision": "1075b042af255ec7d9eccfdd505f7a65",
+    "url": "/react-portfolio/static/media/salvage.1075b042.gif"
+  },
+  {
+    "revision": "5d010597939fbd31c6ddb60e73398567",
+    "url": "/react-portfolio/static/media/mothers-wrath.5d010597.png"
+  },
+  {
+    "revision": "afcb40e79b05ac880aaad67ffd5c68c9",
+    "url": "/react-portfolio/static/media/shattered-core.afcb40e7.png"
+  },
+  {
+    "revision": "106c7e0d531f3140eff52323839e7a87",
+    "url": "/react-portfolio/static/media/santa-cruzin.106c7e0d.png"
+  },
+  {
+    "revision": "20101605253e726f4fe1160b291a797c",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c32072e6392820bf8abdf2c7fa7f8ca.js"
+  "/react-portfolio/precache-manifest.e96ba6f89481e626d9f465dbd4d5d4de.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
