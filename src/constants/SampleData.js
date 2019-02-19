3c178ae0fe2f5811c039c3e44da9b00aaf83028a
Jon Holtan <jonholtan@Alexanders-MacBook-Air.local>
2019-02-19 15:08:27 -0800

    Added iheartRadio, aaa, wonderland. Added tags for future filtering

--- src/constants/SampleData.js
+++ src/constants/SampleData.js
@@ -12,90 +12,112 @@ import santaCruzin from '../images/projectGallery/santa-cruzin.png'
 import troupeAi from '../images/projectGallery/troupe-ai.jpg'
 import iheartRadio from '../images/projectGallery/iheart-radio.jpg'
 import AAA from '../images/projectGallery/aaaLogo.png'
+import wonderland from '../images/projectGallery/wonderland-img.png'
 
 const SampleData = {
   projects: [
     {
       logo: iheartRadio,
       title: 'iHeart Radio',
+      tags: ['android', 'tv'],
       shortDescription:
         "Overhauled iHeart Radio's OTT experiences for FireTV & AndroidTV. Both apps have a 4.0+ rating."
     },
     {
+      logo: wonderland,
+      title: "Queen's Revenge",
+      tags: ['voice', 'game'],
+      shortDescription:
+        'Built the beta application for Troupe.ai which was used for conference attendees of TedX Toronto.'
+    },
+    {
       logo: troupeAi,
       title: 'Troupe.ai',
+      tags: ['android'],
       shortDescription:
         'Built the beta application for Troupe.ai which was used for conference attendees of TedX Toronto.'
     },
     {
       logo: rxJavaTalk,
       title: 'RxJava 2',
+      tags: ['talk', 'android'],
       shortDescription:
         'Lunch & Learn presentation about the basics and effectiveness of the Rx paradim.'
     },
     {
       logo: robot,
       title: 'Robot Rampage',
+      tags: ['game', 'unity'],
       shortDescription:
         'Defeat as many robots as possible within the time limit. Rise the ranks fo the leaderboard! Made in Unity.'
     },
     {
       logo: AAA,
       title: 'AAA Android',
+      tags: ['android'],
       shortDescription:
         'Trained AAA engineers in Android in order to revamp and deploy major updates to their mobile application.'
     },
     {
       logo: mineShaft,
       title: 'Mineshaft Explosion',
+      tags: ['3D'],
       shortDescription:
         'Wanna know what happens when a mineshaft blowsup? Watch and see! Made in Cinema4D.'
     },
     {
       logo: planetExpress,
       title: 'Planet Express',
+      tags: ['3D'],
       shortDescription:
         'Good news everyone! Planet Express HQ is now available in Low Poly Format. Made in Cinema4D.'
     },
     {
       logo: android,
       title: 'Android Testing',
+      tags: ['android', 'talk'],
       shortDescription:
         'An introduction to testing Android using Mockito and common concepts.'
     },
     {
       logo: shatteredCore,
       title: 'Shattered Core',
+      tags: ['3D'],
       shortDescription:
         'A mystical core rises from the ground controlling the warped water in this oasis. Made in Cinema4D.'
     },
     {
       logo: custom,
       title: 'Custom Views',
+      tags: ['talk', 'android'],
       shortDescription:
         "Discover how to use Custom Views as an alternative to Android's Fragments."
     },
     {
       logo: militarium,
       title: 'Militarium',
+      tags: ['game', 'c++'],
       shortDescription:
         'GO inspired RTS made with a custom c++ engine and player achievement system.'
     },
     {
       logo: mothersWrath,
       title: "Mother's Wrath",
+      tags: ['game', 'c#'],
       shortDescription:
         'Play as Mother Earth who has risen to take back Earth from the clutches of humanity.'
     },
     {
       logo: santaCruzin,
       title: 'Santa Cruzin',
+      tags: ['game', 'c#'],
       shortDescription:
         'Descend the mountains of Santa Cruz in this 3D Kinect for Windows game. Made C#/XNA.'
     },
     {
       logo: salvage,
       title: 'Salvage',
+      tags: ['game', 'gamemaker'],
       shortDescription:
         'Collect fuel cells and use space junk to defeat enemies. Made in GameMaker.'
     }
