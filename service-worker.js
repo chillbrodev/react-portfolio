4929ff3bb414134d587a1abfe4d872e22e5cf29e
Jon Holtan <jonholtan@Alexanders-MacBook-Air.local>
2019-03-15 14:47:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.99811b0c7705082e588398e3d08005e9.js"
+  "http://react-website-client.s3-website-us-west-2.amazonaws.com/precache-manifest.f0c062c3822413d01b7ee9aa39cbec62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("http://react-website-client.s3-website-us-west-2.amazonaws.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
